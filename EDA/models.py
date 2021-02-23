31e5f85b05e42ba0c2f91482d0f6a5886cb210e3
ObaidKhan625 <ephiram2002@gmail.com>
2021-02-23 13:13:09 +0530

    Inserted all the tuples inside functions for cleaner view

--- EDA/models.py
+++ EDA/models.py
@@ -1,21 +1,28 @@
 from django.db import models
 import pandas as pd
 
-# Create your models here.
-class BarPlotModel(models.Model):
+def accept_csv():
 	df = pd.read_csv('EDA/diamonds.csv')
+	return df
+
+def estimator_tuples():
+	estimator_tuple = (
+		('mean', 'mean'),
+		('median', 'median'),
+		)
+	return estimator_tuple
+
+def feature_tuples(df):
 	single_list = df.columns
 	big_list = []
 	for i in single_list:
 		if i != 'Unnamed: 0':
 			big_list.append(tuple([i, i]))
 	feature_tuple = tuple(big_list)
-	print(feature_tuple)
-	estimator_tuple = (
-		('mean', 'mean'),
-		('median', 'median'),
-		)
-	palette_choices = (
+	return feature_tuple
+
+def palette_choices():
+	palette_choice = (
 		('rocket', 'rocket'),
 		('mako', 'mako'),
 		('flare', 'flare'),
@@ -26,61 +33,47 @@ class BarPlotModel(models.Model):
 		('inferno', 'inferno'),
 		('hot', 'hot'),
 		)
+	return palette_choice
+
+def legend_choices():
+	legend_choice = (
+		('auto', 'auto'),
+		('brief', 'brief'),
+		('full', 'full'),
+		(False, False),
+		)
+	return legend_choice
+
+# Create your models here.
+class BarPlotModel(models.Model):
+	df = accept_csv()
 	name = 'Bar Plot'
-	x = models.CharField(max_length=200, null=True, choices=feature_tuple)
-	y = models.CharField(max_length=200, null=True, choices=feature_tuple)
-	hue = models.CharField(max_length=200, null=True, choices=feature_tuple, blank=True)
-	estimator = models.CharField(max_length=200, default='mean', blank=True, choices=estimator_tuple)
+	x = models.CharField(max_length=200, null=True, choices=feature_tuples(df))
+	y = models.CharField(max_length=200, null=True, choices=feature_tuples(df))
+	hue = models.CharField(max_length=200, null=True, choices=feature_tuples(df), blank=True)
+	estimator = models.CharField(max_length=200, default='mean', blank=True, choices=estimator_tuples())
 	saturation = models.FloatField(max_length=200, null=True, blank=True) #0 to 1 1 = dark
 	errcolor = models.CharField(max_length=200, default='0.26', blank=True) #0 to 1 1 = white
 	errwidth = models.FloatField(max_length=200, null=True, blank=True) # 1-3 recomm
 	capsize = models.FloatField(max_length=200, null=True, blank=True) # 0 to 0.4 recomm
 	color = models.CharField(max_length=200, null=True, blank=True)
 	n_boot = models.IntegerField(null=True, blank=True) # number of iter to cal conf int
-	palette = models.CharField(max_length=200, blank=True, null=True, choices=palette_choices)
+	palette = models.CharField(max_length=200, blank=True, null=True, choices=palette_choices())
 	
 	def __str__(self):
 		return self.name
 
 class ScatterPlotModel(models.Model):
-	df = pd.read_csv('EDA/diamonds.csv')
-	single_list = df.columns
-	big_list = []
-	for i in single_list:
-		if i != 'Unnamed: 0':
-			big_list.append(tuple([i, i]))
-	feature_tuple = tuple(big_list)
-	print(feature_tuple)
-	estimator_tuple = (
-		('mean', 'mean'),
-		('median', 'median'),
-		)
-	palette_choices = (
-		('rocket', 'rocket'),
-		('mako', 'mako'),
-		('flare', 'flare'),
-		('crest', 'crest'),
-		('magma', 'magma'),
-		('viridis', 'viridis'),
-		('icefire', 'icefire'),
-		('inferno', 'inferno'),
-		('hot', 'hot'),
-		)
-	legend_choices = (
-		('auto', 'auto'),
-		('brief', 'brief'),
-		('full', 'full'),
-		(False, False),
-		)
+	df = accept_csv()
 	name = 'Scatter Plot'
-	x = models.CharField(max_length=200, null=True, choices=feature_tuple)
-	y = models.CharField(max_length=200, null=True, choices=feature_tuple)
-	hue = models.CharField(max_length=200, null=True, choices=feature_tuple, blank=True)
-	estimator = models.CharField(max_length=200, default='none', choices=estimator_tuple)
-	style = models.CharField(max_length=200, null=True, choices=feature_tuple) #Different shapes of scattered points
-	sizes = models.CharField(max_length=200, null=True, choices=feature_tuple) #Different sizes of scattered points
-	palette = models.CharField(max_length=200, blank=True, null=True, choices=palette_choices)
-	legend = models.CharField(max_length=200, default='auto', choices=legend_choices)
+	x = models.CharField(max_length=200, null=True, choices=feature_tuples(df))
+	y = models.CharField(max_length=200, null=True, choices=feature_tuples(df))
+	hue = models.CharField(max_length=200, null=True, choices=feature_tuples(df), blank=True)
+	estimator = models.CharField(max_length=200, default='none', choices=estimator_tuples())
+	style = models.CharField(max_length=200, null=True, choices=feature_tuples(df)) #Different shapes of scattered points
+	sizes = models.CharField(max_length=200, null=True, choices=feature_tuples(df)) #Different sizes of scattered points
+	palette = models.CharField(max_length=200, blank=True, null=True, choices=palette_choices())
+	legend = models.CharField(max_length=200, default='auto', choices=legend_choices())
 	n_boot = models.IntegerField(null=True, blank=True) # number of iter to cal conf int
 	
 	def __str__(self):
--- EDA/prac.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-
-df = pd.read_csv('housing.csv')
-X = df['total_rooms']
-y = df['median_house_value']
-plt.scatter(X, y)
-plt.savefig('C:/Users/OBAID/OneDrive/Desktop/MP/static/images/plot.png')
