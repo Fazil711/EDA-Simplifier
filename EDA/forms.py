a959ee22978491281780f192455815bb16490fb8
Fazil Ahmed Shaikh <44942652+Fazil711@users.noreply.github.com>
2021-03-07 23:14:31 +0530

    Update forms.py

--- EDA/forms.py
+++ EDA/forms.py
@@ -30,3 +30,33 @@ class HistogramPlotForm(ModelForm):
 	class Meta:
 		model = HistogramPlotModel
 		fields = '__all__'
+
+class BarPlotImageForm(ModelForm):
+	class Meta:
+		model = BarPlotImageModel
+		fields = '__all__'
+
+class ScatterPlotImageForm(ModelForm):
+	class Meta:
+		model = ScatterPlotImageModel
+		fields = '__all__'
+
+class LinePlotImageForm(ModelForm):
+	class Meta:
+		model = LinePlotImageModel
+		fields = '__all__'
+		
+class BoxPlotImageForm(ModelForm):
+	class Meta:
+		model = BoxPlotImageModel
+		fields = '__all__'
+
+class CountPlotImageForm(ModelForm):
+	class Meta:
+		model = CountPlotImageModel
+		fields = '__all__'
+		
+class HistogramPlotImageForm(ModelForm):
+	class Meta:
+		model = HistogramPlotImageModel
+		fields = '__all__'
