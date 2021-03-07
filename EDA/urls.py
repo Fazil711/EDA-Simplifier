5269d88b21ab383768c6fe0544f84ca3015d9ef1
Fazil Ahmed Shaikh <44942652+Fazil711@users.noreply.github.com>
2021-03-07 23:15:50 +0530

    Update urls.py

--- EDA/urls.py
+++ EDA/urls.py
@@ -11,4 +11,10 @@ urlpatterns = [
     path('histogram_plot_form/', views.HistogramPlotFormPage, name='histogram_plot_form_page'),
     path('box_plot_form/', views.BoxPlotFormPage, name='box_plot_form_page'),
     path('delete_graph/', views.deleteGraph, name="delete_graph"),
+    path('bar_plot_image_form/', views.BarPlotImageFormPage, name='bar_plot_image_form_page'),
+    path('scatter_plot_image_form/', views.BarPlotImageFormPage, name='scatter_plot_image_form_page'),
+    path('line_plot_image_form/', views.BarPlotImageFormPage, name='line_plot_image_form_page'),
+    path('count_plot_image_form/', views.BarPlotImageFormPage, name='count_plot_image_form_page'),
+    path('histogram_plot_image_form/', views.BarPlotImageFormPage, name='histogram_plot_image_form_page'),
+    path('box_plot_image_form/', views.BarPlotImageFormPage, name='box_plot_image_form_page'),
 ]
